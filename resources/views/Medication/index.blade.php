cdcb1b25a941fe2f8808a60e56186a5dbe86da80
aarongreendev <aarongreen@outlook.com>
2021-04-21 23:11:05 +0100

    added expiry_date filter

--- resources/views/Medication/index.blade.php
+++ resources/views/Medication/index.blade.php
@@ -33,24 +33,33 @@
                     class="cursor-pointer px-5 py-3 bg-indigo-300 text-white text-center inline-block hover:opacity-75 hover:shadow hover:-mb-3 rounded-t"> Filters</h4>
                     <div x-show="selected == 0" class="border py-4 px-2">
                         <div class="relative">
-                        <div class="mt-5 md:mt-1 md:col-span-2">
-                        <div class="col-span-6 sm:col-span-3">
-                            <select class="block appearance-none w-auto bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                                <option>Cannock</option>
-                                <option>Essex</option>
-                            </select>
+                            <div class="mt-5 md:mt-1 md:col-span-2">
+                             
+                                    <select class="block appearance-none w-auto bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                                        <option>Cannock</option>
+                                        <option>Essex</option>
+                                    </select>
+                             
+                               
+                                    <select class="block appearance-none w-auto bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                                        <option>In Date</option>
+                                        <option>Expired</option>
+                                    </select>
+
+                                    <select class="block appearance-none w-auto bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                                        <option>Expires: Next Week</option>
+                                        <option>Expires: Next Month</option>
+                                        <option>Expires: Next Year</option>
+                                    </select>
+                                
+                                
+                                    <div class="p-1">  
+                                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded content-end">
+                                            Filter
+                                        </button>
+                                    </div>  
+                                
                             </div>
-                            <div class="col-span-6 sm:col-span-3">
-                            <div class="p-1">  
-                                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded content-end">
-                                    Filter
-                                </button>
-                            </div>  
-                            </div>
-                            </div>
-                            <!--<div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-                            </div>-->
                         </div>
                     </div>
                 </li>
