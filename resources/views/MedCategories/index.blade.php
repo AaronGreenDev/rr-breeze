63cb0596df5bd9b65f0142858d052e898d6aebcb
aarongreendev <aarongreen@outlook.com>
2021-04-30 11:18:15 +0100

    added collapsable boxes

--- resources/views/MedCategories/index.blade.php
+++ resources/views/MedCategories/index.blade.php
@@ -2,6 +2,71 @@
 
 @section('content')
 
+<style>
+         /* Tab content - closed */
+         .tab-content {
+         max-height: 0;
+         -webkit-transition: max-height .35s;
+         -o-transition: max-height .35s;
+         transition: max-height .35s;
+         }
+         /* :checked - resize to full height */
+         .tab input:checked ~ .tab-content {
+         max-height: 100vh;
+         }
+         /* Label formatting when open */
+         .tab input:checked + label{
+         /*@apply text-xl p-5 border-l-2 border-indigo-500 bg-gray-100 text-indigo*/
+         font-size: 1.25rem; /*.text-xl*/
+         padding: 1.25rem; /*.p-5*/
+         border-left-width: 2px; /*.border-l-2*/
+         border-color: #6574cd; /*.border-indigo*/
+         background-color: #f8fafc; /*.bg-gray-100 */
+         color: #6574cd; /*.text-indigo*/
+         }
+         /* Icon */
+         .tab label::after {
+         float:right;
+         right: 0;
+         top: 0;
+         display: block;
+         width: 1.5em;
+         height: 1.5em;
+         line-height: 1.5;
+         font-size: 1.25rem;
+         text-align: center;
+         -webkit-transition: all .35s;
+         -o-transition: all .35s;
+         transition: all .35s;
+         }
+         /* Icon formatting - closed */
+         .tab input[type=checkbox] + label::after {
+         content: "+";
+         font-weight:bold; /*.font-bold*/
+         border-width: 1px; /*.border*/
+         border-radius: 9999px; /*.rounded-full */
+         border-color: #b8c2cc; /*.border-grey*/
+         }
+         .tab input[type=radio] + label::after {
+         content: "\25BE";
+         font-weight:bold; /*.font-bold*/
+         border-width: 1px; /*.border*/
+         border-radius: 9999px; /*.rounded-full */
+         border-color: #b8c2cc; /*.border-grey*/
+         }
+         /* Icon formatting - open */
+         .tab input[type=checkbox]:checked + label::after {
+         transform: rotate(315deg);
+         background-color: #6574cd; /*.bg-indigo*/
+         color: #f8fafc; /*.text-grey-lightest*/
+         }
+         .tab input[type=radio]:checked + label::after {
+         transform: rotateX(180deg);
+         background-color: #6574cd; /*.bg-indigo*/
+         color: #f8fafc; /*.text-grey-lightest*/
+         }
+      </style>
+
 <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Stock List') }}
@@ -84,47 +149,78 @@
             <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                     <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                        <table class="min-w-full divide-y divide-gray-200">
-                        <thead class="bg-gray-50">
-                            <tr>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    ID
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500  tracking-wider">
-                                    @sortablelink('category_name', 'Category Name')
-                                </th>
-                               
-                            </tr>
-                        </thead>
-                       
+                        
                             @foreach ($med_categories as $med_category)
-                            <tbody class="bg-blue-300 divide-y divide-gray-200">
-                                <tr>
-                                    <td class="px-6 py-4 whitespace-nowrap">
-                                        <div class="flex items-center">
-                                            <div class="text-sm font-medium text-white">
-                                                {{ $med_category->id }}
+
+                            <div class="w-full md:w-3/5 mx-auto p-8">
+                                        <p></p>
+                                        <div class="shadow-md">
+                                            <div class="tab w-full overflow-hidden border-t">
+                                           
+                                                <input class="absolute opacity-0 " id="tab-multi-one" type="checkbox" name="tabs">
+                                                
+                                                <label class="block p-5 leading-normal cursor-pointer" for="tab-multi-one"> {{ $med_category->category_name }} </label>
+                                                
+                                                    <div class="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
+                                                    <table>
+                                                    @if($med_category->meds)
+                                                  
+                                                        <thead class="bg-gray-50">
+                                                            <tr>
+                                                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                                    ID
+                                                                </th>
+                                                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500  tracking-wider">
+                                                                    @sortablelink('category_name', 'Medicine')
+                                                                </th>
+                                                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500  tracking-wider">
+                                                                    @sortablelink('expiry_date', 'Expiry Date')
+                                                                </th>
+                                                
+                                                            </tr>
+                                                        </thead>
+                                                        @foreach ($med_category->meds as $med)
+                                                            <tr>
+                                                                <td class="px-6 py-4 whitespace-nowrap">
+                                                                    <div class="flex items-center">
+                                                                        <div class="text-sm font-medium text-gray-900">
+                                                                            {{ $med->medicine_id }}
+                                                                        </div>
+                                                                    </div>
+                                                                </td>
+
+                                                                <td class="px-6 py-4 whitespace-nowrap">
+                                                                    <div class="text-sm text-gray-500">
+                                                                            {{ $med->med_name }}
+                                                                    </div>
+                                                                </td>
+
+                                                                <td class="px-6 py-4 whitespace-nowrap">
+                                                                    <div class="text-sm text-gray-500">
+                                                                            {{ $med->expiry_date }}
+                                                                    </div>
+                                                                </td>
+                                                            </td>
+                                                    
+                                                        @endforeach
+                                                    @endif
+                                                    </table>
+                                                    </div>
                                             </div>
                                         </div>
-                                    </td>
-
-                                    <td class="px-6 py-4 whitespace-nowrap">
-                                        <div class="text-sm text-white">
-                                                
-                                                {{ $med_category->category_name }}
-                                        </div>
-                                    </td>
-                                    <td class="px-6 py-4 whitespace-nowrap">
+                                    </div>
+                                   
+                                  
                                         <button title="edit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 p-3 rounded">
                                                 <a href="{{ route('med_category.edit', $med_category->id) }}">
                                                     Edit
                                                 </a>
                                         </button>
-                                    </td>
+                                    
 
-                                </td>
+                                
 
-                                    <td>
+                                    
                                        
                                         
                                           
@@ -133,54 +229,17 @@
 
 
                                  
-                                    </td>
-                                    </tr>
+                                    
+                                    
+
                             
                          
 
-                                    @if($med_category->meds)
-                                        <thead class="bg-gray-50">
-                                            <tr>
-                                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                    ID
-                                                </th>
-                                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500  tracking-wider">
-                                                    @sortablelink('category_name', 'Medicine')
-                                                </th>
-                                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500  tracking-wider">
-                                                    @sortablelink('expiry_date', 'Expiry Date')
-                                                </th>
-                                
-                                            </tr>
-                                        </thead>
-                                        @foreach ($med_category->meds as $med)
-                                            <tr>
-                                                <td class="px-6 py-4 whitespace-nowrap">
-                                                    <div class="flex items-center">
-                                                        <div class="text-sm font-medium text-gray-900">
-                                                            {{ $med->medicine_id }}
-                                                        </div>
-                                                    </div>
-                                                </td>
-
-                                                <td class="px-6 py-4 whitespace-nowrap">
-                                                    <div class="text-sm text-gray-500">
-                                                            {{ $med->med_name }}
-                                                    </div>
-                                                </td>
-
-                                                <td class="px-6 py-4 whitespace-nowrap">
-                                                    <div class="text-sm text-gray-500">
-                                                            {{ $med->expiry_date }}
-                                                    </div>
-                                                </td>
-                                            </td>
-                                        @endforeach
-                                    @endif
+                                    
                             @endforeach
 
-                                        </tbody>
-                        </table>
+                                       
+                        
 
                             
                     </div>
@@ -188,6 +247,24 @@
             </div>
         </div>
     </div>        
-</div>                     
+</div>  
+
+<script>
+      /* Optional Javascript to close the radio button version by clicking it again */
+      var myRadios = document.getElementsByName('tabs2');
+      var setCheck;
+      var x = 0;
+      for(x = 0; x < myRadios.length; x++){
+          myRadios[x].onclick = function(){
+              if(setCheck != this){
+                   setCheck = this;
+              }else{
+                  this.checked = false;
+                  setCheck = null;
+          }
+          };
+      }
+   </script>
+
 
 @endsection
