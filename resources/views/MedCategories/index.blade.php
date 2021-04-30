d7a4dc77a6a5ef60bcecb1e70cc93f8deae9b2db
aarongreendev <aarongreen@outlook.com>
2021-04-30 12:31:33 +0100

    styled dropdowns

--- resources/views/MedCategories/index.blade.php
+++ resources/views/MedCategories/index.blade.php
@@ -159,7 +159,9 @@
                                            
                                                 <input class="absolute opacity-0 " id="tab-multi-{{ $med_category->id }} " type="checkbox" name="{{ $med_category->id }} tabs">
                                                 
-                                                <label class="block p-5 leading-normal cursor-pointer" for="tab-multi-{{ $med_category->id }} "> {{ $med_category->category_name }} </label>
+                                                <label class="block p-5 leading-normal cursor-pointer" for="tab-multi-{{ $med_category->id }} "> <strong>{{ $med_category->category_name }}</strong> &#13;
+                                                Expires Soonest: &#13;
+                                                Quantity:</label>
                                                 
                                                     <div class="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
                                                     <table>
