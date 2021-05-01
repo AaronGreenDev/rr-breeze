dcbdd9a564a9f011b5f29da4aebd39e387c5b4aa
aarongreendev <aarongreen@outlook.com>
2021-05-01 13:38:50 +0100

    added crud controller and views for MedName

---
+++ app/Http/Controllers/MedNameController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MedNameController extends Controller
+{
+    public function index()
+    {
+       //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('medname.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            //'id' => 'required',
+            'name' => 'required',
+        ]);
+
+        MedName::create($request->all());
+
+        return redirect()->route('medname.index')
+            ->with('success', 'Medicine Name added successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        // return view('meds.edit', compact('med'));
+
+       $med_name = MedName::findOrFail($id);
+
+       return view('medname.edit', compact('med_name'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MedName $med_name)
+    {
+        $request->validate([
+            'name' => 'required',
+        
+        ]);
+        $med_name->update($request->all());
+
+        return redirect()->route('medname.index')
+            ->with('success', 'Medicine Name updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MedName $med_name)
+    {
+        
+        $med_name->delete();
+
+        return redirect()->route('medname.index')->with('success','You have successfully deleted a Category!');
+    }
+
+}
