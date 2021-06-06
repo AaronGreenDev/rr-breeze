f736b207689479bebab6d21e55688509777b9bf6
aarongreendev <aarongreen@outlook.com>
2021-06-07 00:29:53 +0100

    added type month to med expiry date input

--- resources/views/Medication/create.blade.php
+++ resources/views/Medication/create.blade.php
@@ -74,7 +74,7 @@
                       <div class="col-span-6 sm:col-span-3">
                    
                         <label for="expiry_date" class="block text-sm font-medium text-gray-700">Expiry Date</label>
-                        <input type="date" name="expiry_date" id="expiry_date" autocomplete="expiry_date" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                        <input type="month" name="expiry_date" id="expiry_date" autocomplete="expiry_date" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
                       </div>
                       
                       <div class="col-span-6 sm:col-span-3">
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -24,10 +24,11 @@
                   
                     <div class="border-t border-gray-200">
                         <div class="flex flex-col">
-                        <button type="button" class="items-center text-center px-2 py-1 border border-transparent rounded-md shadow-sm text-sm font-medium 
-                        text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        Go
-                    </button>
+                        <button title="add" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 p-3 rounded">
+                                <a href="{{ route('packs.index') }}">
+                                    Go
+                                </a>
+                            </button>
 
                         </div>
                     </div>
@@ -38,7 +39,7 @@
                 <div class="bg-pink-500 shadow overflow-hidden sm:rounded-lg">
                     <div class="px-4 py-5 sm:px-6">
                             <h3 class="text-lg leading-6 font-medium text-white">
-                                Restock
+                                Restock Medication
                             </h3>
                          
                         </div>
@@ -46,10 +47,11 @@
                         <div class="border-t border-gray-200">
                             <div class="flex flex-col">
                                 
-                            <button type="button" class="items-center text-center px-2 py-1 border border-transparent rounded-md shadow-sm text-sm font-medium 
-                            text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            Go
-                        </button>
+                            <button title="add" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 p-3 rounded">
+                                <a href="{{ route('med_category.index') }}">
+                                    Go
+                                </a>
+                            </button>
                             </div>
                         </div>
                     </div>
