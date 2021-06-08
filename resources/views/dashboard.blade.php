105b6a33cd62705d8ad27d01ab258b06e9b784b1
aarongreendev <aarongreen@outlook.com>
2021-06-08 16:46:51 +0100

    restyled dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -57,8 +57,83 @@
                     </div>
                 </div>
                 </div>
+        
+
+
+            <div class="main-content flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5">
+
+
+            <div class="flex flex-wrap">
+                <div class="w-full md:w-1/2 xl:w-1/3 p-6">
+                    <!--Metric Card-->
+                    <div class="bg-pink-500 border-b-4 border-pink-500 rounded-lg shadow-xl p-5">
+                        <div class="flex flex-row items-center">
+                            <div class="flex-shrink pr-4">
+                                <div class="rounded-full p-5 bg-blue-600"><i class="fa fa-archive fa-2x fa-inverse"></i></div>
+                            </div>
+                            <div class="flex-1 text-right md:text-center">
+                                <h5 class="font-bold uppercase text-white">Total Stock</h5>
+                                <h3 class="font-bold text-3xl">256 Medications <span class="text-green-500"></span></h3>
+                            </div>
+                        </div>
+                    </div>
+                    <!--/Metric Card-->
+                </div>
+                <div class="w-full md:w-1/2 xl:w-1/3 p-6">
+                    <!--Metric Card-->
+                    <div class="bg-pink-500 border-b-4 border-pink-500 rounded-lg shadow-xl p-5">
+                        <div class="flex flex-row items-center">
+                            <div class="flex-shrink pr-4">
+                                <div class="rounded-full p-5 bg-blue-600"><i class="fas fa-archive fa-2x fa-inverse"></i></div>
+                            </div>
+                            <div class="flex-1 text-right md:text-center">
+                                <h5 class="font-bold uppercase text-white">Expired</h5>
+                                <h3 class="font-bold text-3xl">3 Medications<span class="text-pink-500"></span></h3>
+                            </div>
+                        </div>
+                    </div>
+                    <!--/Metric Card-->
+                </div>
+                <div class="w-full md:w-1/2 xl:w-1/3 p-6">
+                    <!--Metric Card-->
+                    <div class="bg-pink-500 border-b-4 border-pink-500 rounded-lg shadow-xl p-5">
+                        <div class="flex flex-row items-center">
+                            <div class="flex-shrink pr-4">
+                                <div class="rounded-full p-5 bg-blue-600"><i class="fas fa-archive fa-2x fa-inverse"></i></div>
+                            </div>
+                            <div class="flex-1 text-right md:text-center">
+                                <h5 class="font-bold uppercase text-white">Due to Expire</h5>
+                                <h3 class="font-bold text-3xl">2 Medications<span class="text-yellow-600"></span></h3>
+                            </div>
+                        </div>
+                    </div>
+                
+                
+                    <!--/Metric Card-->
+                </div>
+
+
+                <div class="w-full md:w-1/2 xl:w-1/3 p-6">
+                    <!--Metric Card-->
+                    <div class="bg-pink-500 border-b-4 border-pink-500 rounded-lg shadow-xl p-5">
+                        <div class="flex flex-row items-center">
+                            <div class="flex-shrink pr-4">
+                                <div class="rounded-full p-5 bg-blue-600"><i class="fas fa-archive fa-2x fa-inverse"></i></div>
+                            </div>
+                            <div class="flex-1 text-right md:text-center">
+                                <h5 class="font-bold uppercase text-white">Packs</h5>
+                                <h3 class="font-bold text-3xl">5 Packs Missing items<span class="text-yellow-600"></span></h3>
+                            </div>
+                        </div>
+                    </div>
+                
+                
+                    <!--/Metric Card-->
+                </div>
             </div>
 
+
+          
 
             
 
@@ -67,7 +142,7 @@
     </div>            
 
 
-
+</div>
 
 
 
